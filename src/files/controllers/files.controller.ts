e5ecd04a8d2284501aef48c65a3188a71674bf20
Heydi <heydimejia@gmail.com>
2023-10-04 19:50:22 -0600

    imagen de usuario y producto

--- src/files/controllers/files.controller.ts
+++ src/files/controllers/files.controller.ts
@@ -1,46 +1,51 @@
-import { BadRequestException, Controller, Get, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Param, Post, Res, UploadedFile, UseInterceptors } from "@nestjs/common";
+import { FileInterceptor } from "@nestjs/platform-express";
+import { diskStorage } from "multer";
+import { fileFilter } from "src/helpers/fileFilter.helper";
+import { fileNamer } from "src/helpers/fileNamer.helper";
 import { FilesService } from "../services/files.services";
-import { FileInterceptor } from '@nestjs/platform-express';
-import { fileFilter } from 'src/helpers/fileFilter.helper';
-import { diskStorage } from 'multer';
-import { fileNamer } from 'src/helpers/fileNamer.helper';
-
-
-
+import { Response } from "express";
 @Controller('files')
 export class FilesController {
-    constructor(private readonly filesService: FilesService) {}
+  constructor(private readonly filesService: FilesService) {}
 
-    @Post('upload')
-    @UseInterceptors(FileInterceptor('file',{
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file', {
+    fileFilter:fileFilter,
 
-      //Llamamos al fileFilter de multer y le asignamos nuestro helper
-      fileFilter: fileFilter,
-
-      storage: diskStorage({
-        destination: './static/products/',
-        filename: fileNamer,
-      }),
-
+    storage: diskStorage({
+      destination: './static/products/',
+      filename:fileNamer
     })
-    )
-    uploadImage(@UploadedFile() file: Express.Multer.File){
-      if (!file){
-        throw new BadRequestException ('Asegurarse que el archivo es una imagen ');
+  
+  }))
 
-      }
-      return{
-        fileNamer: file. filename,
 
-      };
-
-      }
-      
-    @Get('products/:imageId')
-    getImage(){
-      return 'Hola mundo';
+  UploadImage(@UploadedFile() file: Express.Multer.File) {
+    if (!file){
+      throw new BadRequestException('Asegurese que el archivo es una imagen')
     }
 
+    const url = `${file.filename}`;
+
+    return {url};
+
+
+  }
+
+  @Get('products/:imageName')
+  findProduct(@Res() res: Response, @Param('imageName') imageName: string) {
+    const path = this.filesService.getStaticImageName(imageName);
+
+    res.sendFile(path);
+  }
+  @Get('users/:imageName')
+  findUser(@Res() res:Response, @Param('imageName') imageName: string){
+      const path = this.filesService.getStaticImageName(imageName);
+
+      //return path;
+      res.sendFile(path);
+  }
+
+  
 }
-
-
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,10 +4,11 @@ import { User } from "./entities/user.entity";
 import { UserController } from "./controllers/users.controller";
 import { UsersService } from "./services/users.service";
 import { Module } from "@nestjs/common";
+import { UserImage } from "./entities/user-image.entity";
 
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, UserImage])],
   controllers: [UserController],
   providers: [UsersService],
 })
